d74e65fab674a12092372f685ed4306c56f8dd7a
Wlodzimierz <kowyako@yandex.ru>
2023-02-04 17:31:49 +0100

    Added initial backend .NET 7 Web API

---
+++ backend/ShopAPI/ShopAPI/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        {
+            
+        }
+
+
+    }
+}

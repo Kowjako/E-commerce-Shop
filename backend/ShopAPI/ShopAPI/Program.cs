40af233bd34e475695a336e6d75294282111c904
Wlodzimierz <kowyako@yandex.ru>
2023-02-05 17:59:14 +0100

    Make our API serve static files

--- backend/ShopAPI/ShopAPI/Program.cs
+++ backend/ShopAPI/ShopAPI/Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddDbContext<StoreContext>(p =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,8 +31,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
+// Our API will serve images inside wwwroot
+app.UseStaticFiles();
 
+app.UseAuthorization();
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();

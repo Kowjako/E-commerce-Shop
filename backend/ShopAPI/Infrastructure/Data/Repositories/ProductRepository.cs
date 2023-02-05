252346d1da9e4a57710d2181bde6b4f0c75da7a6
Wlodzimierz <kowyako@yandex.ru>
2023-02-05 15:05:18 +0100

    Cosmetic changes

--- backend/ShopAPI/Infrastructure/Data/Repositories/ProductRepository.cs
+++ backend/ShopAPI/Infrastructure/Data/Repositories/ProductRepository.cs
@@ -8,33 +8,22 @@ namespace Infrastructure.Data.Repositories
     {
         private readonly StoreContext _dbContext;
 
-        public ProductRepository(StoreContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
+        public ProductRepository(StoreContext dbContext) => _dbContext = dbContext;
+        
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
-        {
-            return await _dbContext.ProductBrand.ToListAsync();
-        }
-
+            => await _dbContext.ProductBrand.ToListAsync();
+        
         public async Task<Product> GetProductByIdAsync(int id)
-        {
-            return await _dbContext.Products.Include(p => p.ProductBrand)
-                                            .Include(p => p.ProductType)
-                                            .FirstOrDefaultAsync(p => p.Id == id);
-        }
-
+            => await _dbContext.Products.Include(p => p.ProductBrand)
+                                        .Include(p => p.ProductType)
+                                        .FirstOrDefaultAsync(p => p.Id == id);
+        
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
-        {
-            return await _dbContext.Products.Include(p => p.ProductBrand)
-                                            .Include(p => p.ProductType)
-                                            .ToListAsync();
-        }
-
+            => await _dbContext.Products.Include(p => p.ProductBrand)
+                                        .Include(p => p.ProductType)
+                                        .ToListAsync();
+        
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
-        {
-            return await _dbContext.ProductTypes.ToListAsync();
-        }
+            => await _dbContext.ProductTypes.ToListAsync();
     }
 }

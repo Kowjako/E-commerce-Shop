8d9a41fa8ebad1516edcae91ac920c1ecb20a413
Wlodzimierz <kowyako@yandex.ru>
2023-02-08 17:16:25 +0100

    Add shared, core and shop-feature angular modules to keep clean workspace

--- frontend/client/src/app/app.component.ts
+++ frontend/client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Pagination } from './models/pagination';
-import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +6,9 @@ import { Product } from './models/product';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'client';
-  products: Product[] = [];
 
-  constructor(private httpClient: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.httpClient.get<Pagination<Product[]>>("https://localhost:5001/api/products?pageSize=50")
-      .subscribe({
-        next: response => this.products = response.data,
-        error: err => console.log(err),
-        complete: () => {
-          console.log("Request completed");
-          console.log("Extra statements")
-        }
-      })
   }
 }

304767cf75f7693b64e4b94ae507b5181f656476
Wlodzimierz <kowyako@yandex.ru>
2023-02-08 18:22:48 +0100

    Add shop component and initial product card component

--- frontend/client/src/app/shop/shop.module.ts
+++ frontend/client/src/app/shop/shop.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductCardComponent
   ],
   imports: [
     CommonModule

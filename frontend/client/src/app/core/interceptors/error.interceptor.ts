826684c0054d57e76df23bf70ff7f3e9ff3c3301
Wlodzimierz <kowyako@yandex.ru>
2023-02-09 12:04:31 +0100

    Add angular interceptor to handle api exceptions

---
+++ frontend/client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router, private toastr: ToastrService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log(error)
+        if(error) {
+          switch(error.status) {
+            case 400: 
+              if(error.error.errors) {
+                throw error.error; //throw Validation Error to component
+              }
+              else {
+                this.toastr.error('Requested resource was not found', error.status.toString());
+              }
+              break;
+            case 401:
+              this.toastr.error('Authorized, you are not', error.status.toString());
+              break;
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+            case 500:
+              const navigationExtras: NavigationExtras = {
+                state: {
+                  error: error.error
+                }
+              }
+              this.router.navigateByUrl('/server-error', navigationExtras);
+              break;
+            default:
+              this.toastr.error('Something unexpected went wrong :(')
+              break;
+          }
+        }
+        return throwError(() => new Error(error.message));
+      })
+    );
+  }
+}

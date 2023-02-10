3372b0ce87a1f5cfa5d38198085377e7ea83b2c5
Wlodzimierz <kowyako@yandex.ru>
2023-02-10 14:01:03 +0100

    Added loading interceptor

--- frontend/client/src/app/core/core.module.ts
+++ frontend/client/src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -19,11 +20,13 @@ import { BreadcrumbModule } from 'xng-breadcrumb';
   imports: [
     CommonModule,
     RouterModule,
-    BreadcrumbModule
+    BreadcrumbModule,
+    NgxSpinnerModule
   ],
   exports: [
     NavBarComponent,
     SectionHeaderComponent,
+    NgxSpinnerModule
   ]
 })
 export class CoreModule { }

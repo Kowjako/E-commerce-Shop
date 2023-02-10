864759d9f4bd95fdbbc5fed184a2426b4d35a465
Wlodzimierz <kowyako@yandex.ru>
2023-02-10 17:10:27 +0100

    Adding some basket classes and add Redis dependency

---
+++ backend/ShopAPI/Infrastructure/Data/Repositories/BasketRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Core.Interface;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        public Task<bool> DeleteBasketAsync(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomerBasket> GetBasketAsync(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- backend/ShopAPI/ShopAPI/Extensions/ApplicationServiceExtensions.cs
+++ backend/ShopAPI/ShopAPI/Extensions/ApplicationServiceExtensions.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShopAPI.Errors;
+using StackExchange.Redis;
 
 namespace ShopAPI.Extensions
 {
@@ -20,8 +21,16 @@ namespace ShopAPI.Extensions
                 p.UseSqlite(config.GetConnectionString("Default"));
             });
 
+            // Setting up Redis
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
+            });
+
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IBasketRepository, BasketRepository>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<ApiBehaviorOptions>(opt =>

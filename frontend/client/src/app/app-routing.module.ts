37fdbd3aeea486c8737638211b310d481a98edcf
Wlodzimierz <kowyako@yandex.ru>
2023-02-11 14:06:27 +0100

    Initial files for checkout module

--- frontend/client/src/app/app-routing.module.ts
+++ frontend/client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BasketModule } from './basket/basket.module';
+import { CheckoutModule } from './checkout/checkout.module';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'server-error', component: ServerErrorComponent },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => ShopModule)},
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => BasketModule)},
+  { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => CheckoutModule)},
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 

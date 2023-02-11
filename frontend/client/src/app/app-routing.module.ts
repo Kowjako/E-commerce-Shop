f12a47a23aa5e29864eb845947d92a4c5b220563
Wlodzimierz <kowyako@yandex.ru>
2023-02-11 11:46:05 +0100

    Initial models and components for basket feature

--- frontend/client/src/app/app-routing.module.ts
+++ frontend/client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasketModule } from './basket/basket.module';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'not-found', component: NotFoundComponent },
   { path: 'server-error', component: ServerErrorComponent },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => ShopModule)},
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => BasketModule)},
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 

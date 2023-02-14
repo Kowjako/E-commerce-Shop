73a0918f103c4a98d217215e41f6eedbecbd3be8
Wlodzimierz <kowyako@yandex.ru>
2023-02-14 09:28:59 +0100

    Added logged user dropdown

--- frontend/client/src/app/account/login/login.component.ts
+++ frontend/client/src/app/account/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AccountService } from '../account.service';
 
 @Component({
@@ -13,11 +14,13 @@ export class LoginComponent {
     password: new FormControl('', Validators.required),
   })
 
-  constructor(private accService: AccountService) {}
+  constructor(private accService: AccountService, private router: Router) {}
 
   onSubmit() {
     this.accService.login(this.loginForm.value).subscribe({
-      next: user => console.log(user)
+      next: user => {
+        this.router.navigateByUrl("/shop")
+      }
     })
   }
 }

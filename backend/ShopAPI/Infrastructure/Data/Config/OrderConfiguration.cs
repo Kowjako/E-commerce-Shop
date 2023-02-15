bdba26daf66ac31164e0d60d154c92f1e5417384
Wlodzimierz <kowyako@yandex.ru>
2023-02-15 09:21:47 +0100

    New migration for orders is coming!!

--- backend/ShopAPI/Infrastructure/Data/Config/OrderConfiguration.cs
+++ backend/ShopAPI/Infrastructure/Data/Config/OrderConfiguration.cs
@@ -13,6 +13,8 @@ namespace Infrastructure.Data.Config
                 a.WithOwner();
             });
 
+            builder.Navigation(p => p.ShipToAddress).IsRequired();
+
             builder.Property(s => s.Status).HasConversion(o => o.ToString(),
                 o => (OrderStatus)Enum.Parse(typeof(OrderStatus), o));
 

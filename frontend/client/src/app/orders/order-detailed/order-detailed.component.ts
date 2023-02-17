840a3df3d630622050b16946c5ad1367ac455d2d
Wlodzimierz <kowyako@yandex.ru>
2023-02-17 12:46:44 +0100

    Add orders table component & redirection from success component

--- frontend/client/src/app/orders/order-detailed/order-detailed.component.ts
+++ frontend/client/src/app/orders/order-detailed/order-detailed.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from 'src/app/shared/models/order';
+import { BreadcrumbService } from 'xng-breadcrumb';
+import { OrdersService } from '../orders.service';
 
 @Component({
   selector: 'app-order-detailed',
   templateUrl: './order-detailed.component.html',
   styleUrls: ['./order-detailed.component.scss']
 })
-export class OrderDetailedComponent {
+export class OrderDetailedComponent implements OnInit {
+  order?: Order;
 
+  constructor(private ordersSvc: OrdersService, private actRoute: ActivatedRoute,
+    private bcService: BreadcrumbService) {}
+
+  ngOnInit(): void {
+    this.loadOrder();
+  }
+
+  private loadOrder() {
+    const orderId = this.actRoute.snapshot.paramMap.get("id");
+    if(orderId) {
+      this.ordersSvc.getOrderById(+orderId).subscribe({
+        next: order => {
+          this.order = order
+          //this.bcService.set("@OrderDetailed", `Order# ${order.id} - ${order.status}`);
+        }
+      })
+    }
+  }
 }

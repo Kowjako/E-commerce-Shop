47c97b3cf7962b8a83c6deb9baedc2b193efda19
Wlodzimierz <kowyako@yandex.ru>
2023-02-18 19:38:00 +0100

    Exposed endpoint from API to accept payment creation

---
+++ backend/ShopAPI/ShopAPI/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Core.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopAPI.Controllers
+{
+    public class PaymentController : BaseApiController
+    {
+        private readonly IPaymentService _paymentSvc;
+
+        public PaymentController(IPaymentService paymentSvc)
+        {
+            _paymentSvc = paymentSvc;
+        }
+
+        [Authorize]
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            return await _paymentSvc.CreateOrUpdatePaymentIntent(basketId);
+        }
+    } 
+}
--- backend/ShopAPI/ShopAPI/DTO/CustomerBasketDTO.cs
+++ backend/ShopAPI/ShopAPI/DTO/CustomerBasketDTO.cs
@@ -8,5 +8,9 @@ namespace ShopAPI.DTO
         public string Id { get; set; }
 
         public List<BasketItemDTO> Items { get; set; }
+
+        public int? DeliveryMethodId { get; set; }
+        public string ClientSecret { get; set; }
+        public string PaymentIntentId { get; set; }
     }
 }

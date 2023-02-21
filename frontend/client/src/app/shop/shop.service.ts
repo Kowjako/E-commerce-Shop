29db9a1400d94b56b42179274b231c8bc388827b
Wlodzimierz <kowyako@yandex.ru>
2023-02-21 10:47:56 +0100

    Caching part 1 - individual products, types and brands - angular side

--- frontend/client/src/app/shop/shop.service.ts
+++ frontend/client/src/app/shop/shop.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { Brand } from '../shared/models/brand';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/product';
@@ -12,6 +12,9 @@ import { Type } from '../shared/models/type';
 })
 export class ShopService {
   baseUrl = "https://localhost:5001/api/";
+  products: Product[] = [];
+  brands: Brand[] = [];
+  types: Type[] = [];
 
   constructor(private httpClient: HttpClient) { }
 
@@ -29,23 +32,50 @@ export class ShopService {
     params = params.append('sort', shopParams.sort)
     params = params.append('pageIndex', shopParams.pageNumber)
     params = params.append('pageSize', shopParams.pageSize)
-    
-    if(shopParams.search) params = params.append('search', shopParams.search)
-    
+
+    if (shopParams.search) params = params.append('search', shopParams.search)
+
     return this.httpClient.get<Pagination<Product[]>>(this.baseUrl + "Products", {
       params
-    });
+    }).pipe(
+      map(resp => {
+        this.products = resp.data
+        return resp;
+      })
+    );
   }
 
   getProduct(id: number): Observable<Product> {
+    const product = this.products.find(x => x.id === id);
+    if (product) {
+      return of(product);
+    }
     return this.httpClient.get<Product>(this.baseUrl + "Products/" + id);
   }
 
   getBrands(): Observable<Brand[]> {
-    return this.httpClient.get<Brand[]>(this.baseUrl + "Products/brands");
+    if(this.brands.length > 0) {
+      return of(this.brands);
+    }
+
+    return this.httpClient.get<Brand[]>(this.baseUrl + "Products/brands").pipe(
+      map(data => {
+        this.brands = data;
+        return data;
+      })
+    );
   }
 
   getTypes(): Observable<Type[]> {
-    return this.httpClient.get<Type[]>(this.baseUrl + "Products/types");
+    if(this.types.length > 0) {
+      return of(this.types)
+    }
+
+    return this.httpClient.get<Type[]>(this.baseUrl + "Products/types").pipe(
+      map(data => {
+        this.types = data;
+        return data;
+      })
+    );
   }
 }
